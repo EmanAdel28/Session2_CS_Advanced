3e96e18d864a8376c1e4f78fb8e353a0015cd803
AmiraAdel28 <amira.adel282001@gmail.com>
2025-01-25 21:41:49 +0200

    Q1

--- Assignment2_Session2_C#_Advanced/Program.cs
+++ Assignment2_Session2_C#_Advanced/Program.cs
@@ -1,10 +1,142 @@
-﻿namespace Assignment2_Session2_C__Advanced
+﻿using System.Xml.Linq;
+
+namespace Assignment2_Session2_C__Advanced
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+
+            #region Q1
+            //1. Given an array  consists of  numbers with size N and number of queries,
+            //in each query you will be given an integer X, and you should print how many
+            //numbers in array that is greater than  X.
+
+            // Method 1 => By Using List
+
+            //bool Flags;
+            //int Size, NumberOfQuery;
+
+            //do
+            //{
+            //    Console.WriteLine("Enter Size Of Array");
+            //    Flags = int.TryParse(Console.ReadLine(), out Size);
+
+            //} while (!Flags || Size <= 0);
+
+            //do
+            //{
+            //    Console.WriteLine("Enter Number Of Query");
+            //    Flags = int.TryParse(Console.ReadLine(), out NumberOfQuery);
+
+            //} while (!Flags || NumberOfQuery <= 0);
+
+            //List<int> array = new List<int>(Size);
+
+            //// Enter Elements of Array
+
+            //Console.WriteLine("Enter Elements of Array");
+            //for (int i = 0; i < Size; i++)
+            //{
+            //    Console.WriteLine($"Enter Element {i + 1} ");
+
+            //    int elements;
+            //    do
+            //    {
+            //        Flags = int.TryParse(Console.ReadLine(), out elements);
+
+            //    } while (!Flags);
+            //    array.Add(elements);
+            //}
+
+            //for (int i = 0; i < NumberOfQuery; i++)
+            //{
+            //    int Number;
+            //    int Count = 0;
+            //    Console.WriteLine($"Query  {i + 1}");
+            //    do
+            //    {
+            //        Flags = int.TryParse(Console.ReadLine(), out Number);
+
+            //    } while (!Flags);
+
+            //    foreach (int element in array)
+            //    {
+            //        if (Number < element)
+            //        {
+            //            Count++;
+
+            //        }
+            //    }
+            //    Console.WriteLine($"Numbers of elements Greter than {Number} = {Count}");
+
+
+            //}
+
+            /////////////////////////////////////////////////////////////////////////////////////
+
+            // Method 2 => By Using Array
+
+            //bool Flags;
+            //int Size, NumberOfQuery;
+
+            //do
+            //{
+            //    Console.WriteLine("Enter Size Of Array");
+            //    Flags = int.TryParse(Console.ReadLine(), out Size);
+
+            //} while (!Flags || Size <= 0);
+
+            //do
+            //{
+            //    Console.WriteLine("Enter Number Of Query");
+            //    Flags = int.TryParse(Console.ReadLine(), out NumberOfQuery);
+
+            //} while (!Flags || NumberOfQuery <= 0);
+
+
+            //int[] array = new int[Size];
+
+            //Console.WriteLine("Enter Elements of Array");
+            //for (int i = 0; i < Size; i++)
+            //{
+            //    Console.WriteLine($"Enter Element {i + 1} ");
+
+            //    int elements;
+            //    do
+            //    {
+            //        Flags = int.TryParse(Console.ReadLine(), out elements);
+
+            //    } while (!Flags);
+            //    array[i] = elements;
+            //}
+
+            //for (int i = 0; i < NumberOfQuery; i++)
+            //{
+            //    int Number;
+            //    int Count = 0;
+            //    Console.WriteLine($"Query  {i + 1}");
+            //    do
+            //    {
+            //        Flags = int.TryParse(Console.ReadLine(), out Number);
+
+            //    } while (!Flags);
+
+            //    foreach (int element in array)
+            //    {
+            //        if (Number < element)
+            //        {
+            //            Count++;
+
+            //        }
+            //    }
+            //    Console.WriteLine($"Numbers of elements Greter than {Number} = {Count}");
+
+
+            //}
+            #endregion
+
         }
     }
-}
+    }
+
